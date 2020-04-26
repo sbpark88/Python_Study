824cae560c1d3dd97353a00871871011650f2e8e
sbpark88 <saebyulpark88@gmail.com>
2020-04-26 17:22:29 +0900

    Added second my function.
    This function can change str format list to integer(also number to str can be changed) or float and calculate sum, average.

---
+++ lecture/strListChangeSumAverage.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# 리스트 내의 숫자를 정수형, 실수형, 문자형으로 자유롭게 바꿀 수 있다.
+# 또한 문자형으로 된 리스트의 합, 평균을 구할 수 있다.
+
+
+# In[27]:
+
+
+# 숫자로 구성된 리스트를 문자로 바꾸는 함수.
+def numberToString(inputList):
+    for i in range(0,len(inputList),):
+        inputList[i] = str(inputList[i])
+    return inputList
+
+
+# In[3]:
+
+
+# 문자로 구성된 리스트를 정수로 바꾸는 함수.
+def stringToInt(inputList):
+    for i in range(0,len(inputList),):
+        inputList[i] = int(inputList[i])
+    return inputList
+
+
+# In[11]:
+
+
+# 문자로 구성된 리스트를 실수로 바꾸는 함수.
+def stringToFloat(inputList):
+    for i in range(0,len(inputList),):
+        inputList[i] = float(inputList[i])
+    return inputList
+
+
+# In[1]:
+
+
+# 문자로 구성된 리스트의 합을 구하는 함수.
+def stringNumberSum(inputList):
+    for i in range(0,len(inputList),):
+        inputList[i] = int(inputList[i])
+    return sum(inputList)
+
+
+# In[24]:
+
+
+# 문자로 구성된 리스트의 평균을 구하는 함수.
+def stringNumberAvg(inputList):
+    for i in range(0,len(inputList),):
+        inputList[i] = int(inputList[i])
+    return sum(inputList) / len(inputList)
+
